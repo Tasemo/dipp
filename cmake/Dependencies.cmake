b3e7deb06e00b5693812e0d39f176004dcfe9f82
Tim Oelkers <tim.oelkers@web.de>
2023-10-02 19:17:22 +0200

    Copy deployment scripts and additional tools to bin directory

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -18,3 +18,15 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${OLD_RUNTIME_DIR}/tools)
 FetchContent_MakeAvailable(thrill)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${OLD_RUNTIME_DIR})
 set_directory_properties(PROPERTIES COMPILE_OPTIONS "${OLD_OPTIONS}")
+
+get_target_property(thrill_srcdir thrill SOURCE_DIR)
+add_custom_target(
+    copy_thrill_scripts ALL
+    COMMAND ${CMAKE_COMMAND} -E copy_directory
+    ${thrill_srcdir}/../run ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/deploy
+    COMMAND ${CMAKE_COMMAND} -E copy
+    ${thrill_srcdir}/../misc/json2graphviz.py ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/tools
+    COMMAND ${CMAKE_COMMAND} -E copy
+    ${thrill_srcdir}/../misc/standalone_profiler_run.sh ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/tools
+    DEPENDS thrill
+)
