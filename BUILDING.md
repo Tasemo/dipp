5ef58d1e96aa6f7bae388ada82c391e7d676f232
Tim Oelkers <tim.oelkers@web.de>
2023-10-02 16:08:02 +0200

    Add doxygen for documentation

--- BUILDING.md
+++ BUILDING.md
@@ -10,7 +10,9 @@ Builds are tested with the latest Ubuntu with GCC and Clang. Adapt package names
 
 When developing, consider installing some of the following tools that are included in the build process. They are optional, but help to maintain a good code quality and consistent formatting.
 
-- clang-format (system package)
+- clang-format (system package or https://github.com/llvm/llvm-project)
+- clang-tidy (system package or https://github.com/llvm/llvm-project)
+- Doxygen (system package or https://github.com/doxygen/doxygen)
 
 ## Building
 
@@ -18,6 +20,7 @@ First, checkout the code from the GitHub repository.
 
 ```shell
 git clone https://github.com/Tasemo/dipp.git
+cd dipp
 ```
 
 When developing or testing, build the code using Debug mode.
--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,5 +1,6 @@
 option(USE_CLANG_FORMAT "Use the clang-format tool if found" ON)
 option(USE_CLANG_TIDY "Use the clang-tidy tool if found" OFF)
+option(USE_DOXYGEN "Use the doxygen tool if found" ON)
 
 if(USE_CLANG_FORMAT)
     find_program(CLANG_FORMAT clang-format)
@@ -19,5 +20,30 @@ if(USE_CLANG_TIDY)
         set(CMAKE_CXX_CLANG_TIDY clang-tidy)
     else()
         message(NOTICE "clang-tidy requested, but not found")
+    endif()
+endif()
+
+if(USE_DOXYGEN)
+    find_package(Doxygen OPTIONAL_COMPONENTS dot)
+
+    if(DOXYGEN_FOUND)
+        include(FetchContent)
+        FetchContent_Declare(
+            doxygen_theme
+            GIT_REPOSITORY https://github.com/jothepro/doxygen-awesome-css
+            GIT_TAG v2.2.1
+        )
+        FetchContent_MakeAvailable(doxygen_theme)
+
+        set(DOXYGEN_QUIET YES)
+        set(DOXYGEN_OUTPUT_DIRECTORY ../docs)
+        set(DOXYGEN_GENERATE_TREEVIEW YES)
+        set(DODYGEN_DISABLE_INDEX NO)
+        set(DOXYGEN_FULL_SIDEBAR NO)
+        set(DOXYGEN_HTML_COLORSTYLE LIGHT)
+        set(DOXYGEN_HTML_EXTRA_STYLESHEET "${doxygen_theme_SOURCE_DIR}/doxygen-awesome.css")
+        doxygen_add_docs(docs src ALL)
+    else()
+        message(NOTICE "doxygen requested, but not found")
     endif()
 endif()
