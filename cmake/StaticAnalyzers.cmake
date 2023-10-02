a0330811ee05cd664a3170757e0a1982040c8667
Tim Oelkers <tim.oelkers@web.de>
2023-10-02 15:30:07 +0200

    Add clang-tidy for static code analysis

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,4 +1,5 @@
 option(USE_CLANG_FORMAT "Use the clang-format tool if found" ON)
+option(USE_CLANG_TIDY "Use the clang-tidy tool if found" OFF)
 
 if(USE_CLANG_FORMAT)
     find_program(CLANG_FORMAT clang-format)
@@ -8,5 +9,15 @@ if(USE_CLANG_FORMAT)
         add_custom_target(format ALL COMMAND ${CLANG_FORMAT} --Wno-error=unknown -i ${SOURCES})
     else()
         message(NOTICE "clang-format requested, but not found")
+    endif()
+endif()
+
+if(USE_CLANG_TIDY)
+    find_program(CLANG_TIDY clang-tidy)
+
+    if(CLANG_TIDY)
+        set(CMAKE_CXX_CLANG_TIDY clang-tidy)
+    else()
+        message(NOTICE "clang-tidy requested, but not found")
     endif()
 endif()
