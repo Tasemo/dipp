615b8a52fa4530196b0ce83ba3a9cd4ed8156893
Tim Oelkers <tim.oelkers@web.de>
2023-10-03 12:44:18 +0200

    Integrates CI pipeline with GitHub Actions

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <loading/sdss_image_loader.hpp>
 #include <thrill/api/context.hpp>
 
-void process(thrill::Context &ctx) {}
-
-int main() {
+void process(thrill::Context &ctx) {
   loading::SDSSImageLoader loader;
   auto image_path = loader.load_image();
+}
+
+int main() {
   return thrill::api::Run([&](thrill::api::Context &ctx) {
     process(ctx);
   });
