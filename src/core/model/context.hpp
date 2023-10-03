62c1cb6ac760c9af5f991d1a7b0f5b48aa40f6b0
Tim Oelkers <tim.oelkers@web.de>
2023-10-03 18:52:32 +0200

    Properly documents context objects

--- src/core/model/context.hpp
+++ src/core/model/context.hpp
@@ -4,14 +4,54 @@
 
 namespace model {
 
+/**
+ * @brief holds context about the distributed image and methods to convert between local and global coordinates and indices
+ */
 struct Context {
+  /**
+   * @brief the thrill context with information about the distribution
+   */
   thrill::Context& ctx;
+  /**
+   * @brief the unique id of the current worker
+   */
   const size_t rank;
-  const size_t global_width, global_height;
-  const size_t x_tiles, y_tiles;
-  const size_t x_tile, y_tile;
-  const size_t local_width, local_height;
+  /**
+   * @brief the global width of the image
+   */
+  const size_t global_width;
+  /**
+   * @brief the global height of the image
+   */
+  const size_t global_height;
+  /**
+   * @brief the amount of subimages in the x direction
+   */
+  const size_t x_tiles;
+  /**
+   * @brief the amount of subimages in the y direction
+   */
+  const size_t y_tiles;
+  /**
+   * @brief the current subimage in the x direction, staring from the top left
+   */
+  const size_t x_tile;
+  /**
+   * @brief the current subimage in the y direction, staring from the top left
+   */
+  const size_t y_tile;
+  /**
+   * @brief the local width of all subimages
+   */
+  const size_t local_width;
+  /**
+   * @brief the local height of all subimages
+   */
+  const size_t local_height;
 
+  /**
+   * @brief Constructs a new context with a given thrill context and a global width and height
+   */
   Context(thrill::Context& ctx, size_t global_width, size_t global_height)
       : ctx(ctx),
         rank(ctx.my_rank()),
@@ -24,38 +64,65 @@ struct Context {
         local_width(global_width / x_tiles),
         local_height(global_height / y_tiles) {}
 
+  /**
+   * @brief transforms a global pixel index to a local one
+   */
   inline size_t to_local_index(size_t index) const {
     return index % (local_width * local_height);
   }
 
+  /**
+   * @brief calculates the local x position of the pixel from a global index
+   */
   inline size_t index_to_local_x(size_t index) const {
     return to_local_index(index) % local_width;
   }
 
+  /**
+   * @brief calculates the local y position of the pixel from a global index
+   */
   inline size_t index_to_local_y(size_t index) const {
     return to_local_index(index) / local_width;
   }
 
+  /**
+   * @brief calculates the global x position of the pixel from a global index
+   */
   inline size_t index_to_global_x(size_t index) const {
     return index_to_local_x(index) + x_tile * local_width;
   }
 
+  /**
+   * @brief calculates the global y position of the pixel from a global index
+   */
   inline size_t index_to_global_y(size_t index) const {
     return index_to_local_y(index) + y_tile * local_height;
   }
 
+  /**
+   * @brief calculates the local x position of a pixel from a global x
+   */
   inline size_t to_local_x(size_t global_x) const {
     return global_x % local_width;
   }
 
+  /**
+   * @brief calculates the local y position of a pixel from a global y
+   */
   inline size_t to_local_y(size_t global_y) const {
     return global_y % local_height;
   }
 
+  /**
+   * @brief calculates the global x position of a pixel from a local x
+   */
   inline size_t to_global_x(size_t local_x) const {
     return local_x + x_tile * local_width;
   }
 
+  /**
+   * @brief calculates the global y position of a pixel from a local y
+   */
   inline size_t to_global_y(size_t local_y) const {
     return local_y + y_tile * local_height;
   }
--- src/core/model/sdss_context.hpp
+++ src/core/model/sdss_context.hpp
@@ -8,15 +8,54 @@ namespace model {
 
 static const double ARCSEC_TO_DEGREE = 1 / 3600.0;
 
+/**
+ * @brief holds context about the SDSS data set and methods to convert celestial coordinates
+ */
 struct SDSSContext {
+  /**
+   * @brief the general context with information about the distributed image
+   */
   const model::Context& context;
+  /**
+   * @brief the scale of how many arcseconds each pixel covers
+   */
   const double scale_arc;
+  /**
+   * @brief the scale of how many degrees each pixel covers
+   */
   const double scale_deg;
-  const double start_ra, start_dec;
-  const double max_ra, max_dec;
-  const double local_max_ra, local_max_dec;
+  /**
+   * @brief the right-ascension of the top left corner of the global image
+   */
+  const double start_ra;
+  /**
+   * @brief the declination of the top left corner of the global image
+   */
+  const double start_dec;
+  /**
+   * @brief the right-ascesion of the top right corner of the global image
+   */
+  const double max_ra;
+  /**
+   * @brief the declination of the top right corner of the global image
+   */
+  const double max_dec;
+  /**
+   * @brief the right-ascesion of the top left corner of the current subimage
+   */
+  const double local_start_ra;
+  /**
+   * @brief the declination of the top left corner of the current subimage
+   */
+  const double local_start_dec;
+  /**
+   * @brief a key to identify unique sdss datasets, based on starting point, size and number of workers
+   */
   const std::string file_key;
 
+  /**
+   * @brief Constructs a new SDSS context with a given general context, the top left starting point and a pixel scale
+   */
   SDSSContext(const model::Context& context, double start_ra, double start_dec, double scale_arc)
       : context(context),
         scale_arc(scale_arc),
@@ -25,8 +64,8 @@ struct SDSSContext {
         start_dec(start_dec),
         max_ra(start_ra - context.global_width * scale_deg),
         max_dec(start_dec - context.global_height * scale_deg),
-        local_max_ra(start_ra - context.x_tile * context.local_width * scale_deg),
-        local_max_dec(start_dec - context.y_tile * context.local_height * scale_deg),
+        local_start_ra(start_ra - context.x_tile * context.local_width * scale_deg),
+        local_start_dec(start_dec - context.y_tile * context.local_height * scale_deg),
         file_key(
             util::to_string(context.ctx.num_workers()) + 'x' +
             util::to_string(start_ra) + 'x' +
@@ -35,10 +74,16 @@ struct SDSSContext {
             util::to_string(context.local_height)
         ) {}
 
+  /**
+   * @brief converts the given right-ascension from the top left to the center of the image
+   */
   inline double to_center_ra(double ra) const {
     return ra - context.local_width / 2.0 * scale_deg;
   }
 
+  /**
+   * @brief converts the given declination from the top left to the center of the image
+   */
   inline double to_center_dec(double dec) const {
     return dec - context.local_height / 2.0 * scale_deg;
   }
