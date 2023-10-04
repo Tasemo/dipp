73eb6998de3e5e10c0b3fead7d4583430810cb52
Tim Oelkers <tim.oelkers@web.de>
2023-10-04 16:00:30 +0200

    Specializes pipeline for void inputs

--- src/core/processing/pipeline.hpp
+++ src/core/processing/pipeline.hpp
@@ -13,9 +13,22 @@ class Processor {
  public:
   virtual ~Processor() = default;
   /**
-   * @brief transforms the input to some output
+   * @brief transforms the input to some output using the context
    */
   virtual Output process(const model::Context &ctx, const Input &data) const = 0;
+};
+
+/**
+ * @brief a specialization of a processor that just produces some output without input
+ */
+template <typename Output>
+class Processor<void, Output> {
+ public:
+  virtual ~Processor() = default;
+  /**
+   * @brief produces some output using the context
+   */
+  virtual Output process(const model::Context &ctx) const = 0;
 };
 
 /**
@@ -78,5 +91,69 @@ class Pipeline : public Processor<Input, Output> {
     return tlx::make_counting<ChainedStage>(*this, next);
   }
 };
+
+/**
+ * @brief a specialization of a pipeline that is a processor without input
+ */
+template <typename Output>
+class Pipeline<void, Output> : public Processor<void, Output> {
+ private:
+  virtual void inc_ref_counter() const {}
+  virtual bool dec_ref_counter() const {
+    return false;
+  }
+
+ public:
+  virtual ~Pipeline() = default;
+
+  /**
+   * @brief adds the processor to the chain by returning a smart pointer to a object representing the transformation
+   *
+   * @note you can only add processors which accept the output of the current stage as input
+   */
+  template <typename NextOutput>
+  auto add_next(const Processor<Output, NextOutput> &next) const {
+    class ChainedStage : public Pipeline<void, NextOutput>, private tlx::ReferenceCounter {
+      friend class tlx::CountingPtr<ChainedStage>;
+
+     private:
+      const Pipeline<void, Output> &_current;
+      const Processor<Output, NextOutput> &_next;
+
+      void inc_ref_counter() const override {
+        inc_reference();
+      }
+
+      bool dec_ref_counter() const override {
+        return dec_reference();
+      }
+
+     public:
+      ChainedStage(const ChainedStage &) = delete;
+      ChainedStage(ChainedStage &&) = delete;
+      ChainedStage &operator=(const ChainedStage &) = delete;
+      ChainedStage &operator=(ChainedStage &&) = delete;
+
+      ChainedStage(const Pipeline<void, Output> &current, const Processor<Output, NextOutput> &next)
+          : _current(current), _next(next) {}
+
+      ~ChainedStage() {
+        if (_current.dec_ref_counter()) {
+          delete &_current;
+        }
+      }
+
+      NextOutput process(const model::Context &ctx) const override {
+        auto result = _current.process(ctx);
+        return _next.process(ctx, std::forward<Output>(result));
+      }
+    };
+    inc_ref_counter();
+    return tlx::make_counting<ChainedStage>(*this, next);
+  }
+};
+
+template <typename Output>
+using Producer = Pipeline<void, Output>;
 
 }  // namespace processing
